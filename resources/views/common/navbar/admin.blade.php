a5556cb80a3460daea54bdd13e43804a6629b0d6
Ken Vilar <kenvilar@gmail.com>
2020-02-01 18:14:16 +0800

    Utilize blade templates

---
+++ resources/views/common/navbar/admin.blade.php
@@ -0,0 +1,27 @@
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        Invoicing <span class="caret"></span>
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">Dashboard</a>
+        <a class="dropdown-item" href="#">Create Invoice</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Recent Invoices</a>
+        <a class="dropdown-item" href="#">Overdue Invoices</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="{{ route('admin.invoicing.settings') }}">Settings</a>
+    </div>
+</li>
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+        Payments <span class="caret"></span>
+    </a>
+
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">Dashboard</a>
+        <a class="dropdown-item" href="#">Log a Payment</a>
+        <a class="dropdown-item" href="#">Recent Payments</a>
+        <a class="dropdown-item" href="#">Overdue Payments</a>
+    </div>
+</li>
