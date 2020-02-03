66288ceff8bbabb9df94a216c2a74314583f564a
Ken Vilar <kenvilar@gmail.com>
2020-02-03 22:37:44 +0800

    Add Customer model

---
+++ database/migrations/2020_02_03_143007_create_customers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('address_1')->nullable();
+            $table->string('address_2')->nullable();
+            $table->string('city')->nullable();
+            $table->string('province')->nullable(); //or state
+            $table->string('country')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone_1')->nullable();
+            $table->string('phone_2')->nullable();
+            $table->string('fax')->nullable();
+            $table->string('contact_person')->nullable();
+            $table->text('settings')->nullable(); //JSON data stored for client specific settings
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,16 +21,21 @@
 </head>
 <body>
     <div id="app">
-        @include('common.navbar-top')
+        @auth()
+            @include('common.navbar-top')
+        @endauth
+
         <main class="py-4">
-            <div class="container-fluid">
+            <div class="container">
                 <!--Breadcrumbs-->
-                <nav aria-label="breadcrumb row">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                        @yield('breadcrumbs')
-                    </ol>
-                </nav>
+                @auth()
+                    <nav aria-label="breadcrumb row">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            @yield('breadcrumbs')
+                        </ol>
+                    </nav>
+                @endauth
                 @yield('content')
             </div>
         </main>
