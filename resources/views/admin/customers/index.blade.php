5279cefd52d6b7900d2c556dfe74166dc6506a25
Ken Vilar <kenvilar@gmail.com>
2020-02-05 00:31:46 +0800

    Add route('admin.customers.create') in customers index.blade.php

--- resources/views/admin/customers/index.blade.php
+++ resources/views/admin/customers/index.blade.php
@@ -22,7 +22,7 @@
                                     Options <span class="caret"></span>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Edit Company Details</a>
+                                    <a class="dropdown-item" href="{{ route('admin.customers.create') }}">Create Customer</a>
                                     {{-- Add more links here --}}
                                 </div>
                             </li>
