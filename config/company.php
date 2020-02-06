f558bad461bdb30747a8ac2e2b21f41fc18d5804
Ken Vilar <kenvilar@gmail.com>
2020-02-06 23:40:48 +0800

    Add company config

---
+++ config/company.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => '123 Widgets',
+    'address' => 'Dipolog-Bypass National Road, Barangay Turno',
+    'city' => 'Dipolog',
+    'province' => 'Zamboanga del Norte',
+    'country' => 'Philippines',
+    'postal_code' => '7100',
+];
