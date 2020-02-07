f35d9a536185b36b2def982365678e45af32d200
Ken Vilar <kenvilar@gmail.com>
2020-02-07 16:37:37 +0800

    Create route for customers/data/all

--- routes/web/admin/customers.php
+++ routes/web/admin/customers.php
@@ -15,3 +15,7 @@ Route::get('/edit/{customer}', 'CustomersController@edit')->name('edit');
 Route::post('/store', 'CustomersController@store')->name('store');
 Route::put('/update/{customer}', 'CustomersController@update')->name('update');
 Route::delete('/delete/{customer}', 'CustomersController@destroy')->name('delete');
+
+Route::prefix('data')->namespace('Data')->group(function () {
+    Route::get('/all', 'CustomersController@all');
+});
