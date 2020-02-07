dc36a38caca1669fdb71f756cfdc5aaadad330e0
Ken Vilar <kenvilar@gmail.com>
2020-02-07 15:27:35 +0800

    Modify invoicing create page

--- resources/views/admin/invoicing/create.blade.php
+++ resources/views/admin/invoicing/create.blade.php
@@ -58,44 +58,132 @@
                             <h5>From</h5>
                             <hr>
 
-                            <div class="form-group">
-                                <label for="">From</label>
-                                <input type="text" class="form-control" name="from"
-                                       value="{{ old('from') ? old('from') : config('company.name') }}">
+                            <div class="form-group row">
+                                <label for="" class="col-3">From</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="from"
+                                           value="{{ old('from') ? old('from') : config('company.name') }}">
+                                </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="">Address</label>
-                                <input type="text" class="form-control" name="address"
-                                       value="{{ old('address') ? old('address') : config('company.address') }}">
+                            <div class="form-group row">
+                                <label for="" class="col-3">Address</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="address"
+                                           value="{{ old('address') ? old('address') : config('company.address') }}">
+                                </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="">Postal Code</label>
-                                <input type="text" class="form-control" name="postal_code"
-                                       value="{{ old('postal_code') ? old('postal_code') : config('company.postal_code') }}">
+                            <div class="form-group row">
+                                <label for="" class="col-3">Postal Code</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="postal_code"
+                                           value="{{ old('postal_code') ? old('postal_code') : config('company.postal_code') }}">
+                                </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="">City</label>
-                                <input type="text" class="form-control" name="city"
-                                       value="{{ old('city') ? old('city') : config('company.city') }}">
+                            <div class="form-group row">
+                                <label for="" class="col-3">City</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="city"
+                                           value="{{ old('city') ? old('city') : config('company.city') }}">
+                                </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="">Province</label>
-                                <input type="text" class="form-control" name="province"
-                                       value="{{ old('province') ? old('province') : config('company.province') }}">
+                            <div class="form-group row">
+                                <label for="" class="col-3">Province</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="province"
+                                           value="{{ old('province') ? old('province') : config('company.province') }}">
+                                </div>
                             </div>
 
-                            <div class="form-group">
-                                <label for="">Country</label>
-                                <input type="text" class="form-control" name="country"
-                                       value="{{ old('country') ? old('country') : config('company.country') }}">
+                            <div class="form-group row">
+                                <label for="" class="col-3">Country</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="country"
+                                           value="{{ old('country') ? old('country') : config('company.country') }}">
+                                </div>
+                            </div>
+
+                            <h5>For <span class="btn btn-sm btn-primary float-right" data-toggle="modal" data-target="#customersModal">Customer</span></h5>
+                            <hr>
+
+                            <div class="form-group row">
+                                <label for="" class="col-3">For</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="for"
+                                           value="{{ old('for') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="" class="col-3">Address</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="for_address"
+                                           value="{{ old('for_address') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="" class="col-3">Postal Code</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="for_postal_code"
+                                           value="{{ old('for_postal_code') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="" class="col-3">City</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="for_city"
+                                           value="{{ old('for_city') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="" class="col-3">Province</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="for_province"
+                                           value="{{ old('for_province') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="" class="col-3">Country</label>
+                                <div class="col-9">
+                                    <input type="text" class="form-control" name="for_country"
+                                           value="{{ old('for_country') }}">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="customersModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Select a C ustomer</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <ul class="list-group">
+                        @foreach(\App\Models\Customer::all() as $customer)
+                            <li class="list-group-item">{{ $customer->name }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
             </div>
         </div>
     </div>
