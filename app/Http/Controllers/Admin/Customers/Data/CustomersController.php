b2e4c20264b79385a4162e2547f9db04dd839946
Ken Vilar <kenvilar@gmail.com>
2020-02-07 16:38:10 +0800

    Creaate Admin/Customers/Data/CustomersController

---
+++ app/Http/Controllers/Admin/Customers/Data/CustomersController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Customers\Data;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomersController extends Controller
+{
+    public function all()
+    {
+        return Customer::query()->select(['id', 'name',])->get();
+    }
+}
