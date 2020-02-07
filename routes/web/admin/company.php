74589a5c4344aab5191be6b3afa10eab75bcdbe4
Ken Vilar <kenvilar@gmail.com>
2020-02-07 20:26:34 +0800

    Add new route for admin/company

---
+++ routes/web/admin/company.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * prefix: admin/company
+ * namespace: Admin\Company
+ */
+
+Route::prefix('data')->namespace('Data')->group(function () {
+    Route::get('/details', 'CompanyDetailsController@details');
+});
